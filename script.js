94dba4c2e7b84c142820f0f282aa535dc93b7a31
averywh13 <awekslerhart@gmail.com>
2025-02-11 13:02:29 -0500

    filler cards

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const updateCalendar = () => {
   const currentYear = currentDate.getFullYear();
   const currentMonth = currentDate.getMonth();
 
-  const firstDay = new Date(currentYear, currentMonth, 0);
-  const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  const firstDay = new Date(currentYear, currentMonth,0);
+  const lastDay = new Date(currentYear,currentMonth + 1, 0);
   const totalDays = lastDay.getDate();
   const firstDayIndex = firstDay.getDay();
   const lastDayIndex = lastDay.getDay();
@@ -21,16 +21,29 @@ const updateCalendar = () => {
   let datesHTML = '';
   
   for(let i = firstDayIndex; i > 0; i--) {
-    const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+    const prevDate = new Date(currentYear,currentMonth, 0 - i + 1);
     datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
   }
   for(let i = 1; i <= totalDays; i++){
-    const date = new Date(currentYear, currentMonth,i);
-    const activeClass = date.toDataString() === new Date().toDateString() ? 'active' : '';
+    const date = new Date(currentYear, currentMonth, i);
+    const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
     datesHTML += `<div class="date ${activeClass}">${i}</div>`;
   }
 
-  for(let i = 1)
+  for(let i = 1; i <= 7 - lastDayIndex; i++) {
+    const nextDate = new Date(currentYear, currentMonth + 1, i);
+    datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`
+  }
+
+  datesElement.innerHTML = datesHTML;
 }
 
-// https://www.youtube.com/watch?v=OcncrLyddAs
+prevBtn.addEventListener('click', () => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  updateCalendar();
+})
+nextBtn.addEventListener('click', () => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  updateCalendar();
+})
+updateCalendar();
